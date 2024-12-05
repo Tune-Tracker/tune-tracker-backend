b0ea46443b026dc2a47ad7fd91c5678d82677f98
Duchul <guscjfqkr9811@gmail.com>
2024-12-05 11:47:04 +0900

    feat(api): node_modules 삭제, 날씨, 전력 스키마 수정

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const weatherRoutes = require('./routes/monthlyWeather');
-const electRoutss = require('./routes/monthlyElect')
+const electRoutes = require('./routes/monthlyElect')
 const app = express();
 
 // MongoDB 연결
@@ -13,10 +12,14 @@ mongoose.connect(MONGO_URI)
     .catch(err => console.error('MongoDB 연결 실패:', err));
 
 // 미들웨어 설정
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+
+//express 내장 미들웨어
+app.use(express.json());
+
 // 라우트 설정
 app.use('/api/weather', weatherRoutes);
-app.use('/api/elect',electRoutss)
+app.use('/api/elect',electRoutes)
 
 // 서버 실행
 const PORT = process.env.PORT || 3000;
