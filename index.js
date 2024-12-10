351360f3e1b0f673c0cd7ea52417942fa8d600c1
Duchul <guscjfqkr9811@gmail.com>
2024-12-10 23:23:34 +0900

    feat(api): 파이썬 서버 연결

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
+const cors = require('cors')
 const weatherRoutes = require('./routes/monthlyWeather');
 const electRoutes = require('./routes/monthlyElect');
 const weatherWeekRoutes = require('./routes/weeklyWeather');
+const predictRoutes = require('./routes/predict')
 const app = express();
+
+app.use(cors());  // 모든 출처에서의 요청을 허용
 
 // MongoDB 연결
 const MONGO_URI = process.env.MONGO_URI;
@@ -17,12 +22,18 @@ mongoose.connect(MONGO_URI)
 
 //express 내장 미들웨어
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//루트 페이지
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));  // public 디렉토리에 index.html
+});
 
 // 라우트 설정
 app.use('/api/weather', weatherRoutes);
 app.use('/api/elect', electRoutes);
 app.use('/api/weatherWeek', weatherWeekRoutes);
-
+app.use('/predict', predictRoutes);
 // 서버 실행
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/predict.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+
+router.post('/predict', async (req, res) => {
+    const { avgTemp, precipitation, avgRhm, season } = req.body;
+
+    try {
+        // FastAPI 서버로 예측 요청 보내기
+        const response = await axios.post('localhost:8000/predict', {
+            avgTemp,
+            precipitation,
+            avgRhm,
+            season
+        });
+
+        // FastAPI에서 받은 예측 결과 반환
+        res.json({
+            predicted_power_usage: response.data.predicted_power_usage
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Error occurred while making prediction' });
+    }
+});
+
+module.exports = router;
